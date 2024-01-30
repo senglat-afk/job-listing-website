b308d8032708f1f7a914333d3d95d1edffbbbf5b
senglat-afk <121607422+senglat-afk@users.noreply.github.com>
2024-01-30 13:20:46 +0000

    added dynamic data and api route

--- app.py
+++ app.py
@@ -1,15 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify 
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-  return"<p>Hello, World!</p>"
-  
-@app.route("/profile")
-def profile():
-  return"<p>Myihtoi Seng Lat Awng</p>"
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary': 'Rs. 10,00,000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Delhi, India',
+    'salary': 'Rs. 15,00,000'
+  },
+  {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Remote'
+  },
+  {
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': 'San Francisco, USA',
+    'salary': '$150,000'
+  }
+]
 
-if __name__ == "__main__":
+@app.route("/")
+def hello_jovian():
+    return render_template('home.html', 
+                           jobs=JOBS, 
+                           company_name='Seng Latt')
+
+@app.route("/api/jobs")
+def list_jobs(): 
+  return jsonify(JOBS)
+
+if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
-  
